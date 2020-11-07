029ef31332f74d029eff8485f34baf3bce282302
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-11-07 14:45:22 +0700

    add try & catch

--- “ServiceMtk-P1-20180140103/IMatematika.cs
+++ “ServiceMtk-P1-20180140103/IMatematika.cs
@@ -12,18 +12,30 @@ namespace _ServiceMtk_P1_20180140103
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         /* output->*/
         int Tambah(int a, int b); //method
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
 
         [DataContract]
+        class  MathFault
+        {
+            [DataMember]
+            public string Kode { get; set; }
+            [DataMember]
+            public string Pesan { get; set; }
+        }
         public class Koordinat
         {
             private int _x, _y; //atribut
